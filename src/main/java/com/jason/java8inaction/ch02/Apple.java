251314604b3635e50dfd98cdb5f30c3e2a38e08a
pplenty <newtype26@naver.com>
2019-04-11 21:36:18 +0900

    add 2.1.1 apple class

---
+++ src/main/java/com/jason/java8inaction/ch02/Apple.java
@@ -0,0 +1,40 @@
+package com.jason.java8inaction.ch02;
+
+/**
+ * @author : yusik
+ * @date : 2019-04-11
+ */
+public class Apple {
+
+    private int weight;
+    private String color;
+
+    public Apple(int weight, String color) {
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Apple{" +
+                "weight=" + weight +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
