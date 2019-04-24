686c61e6b956488dac2e5a9fc7317cd1543f3e6a
pplenty <newtype26@naver.com>
2019-04-24 19:14:50 +0900

    gradle build 완료:cglib

--- src/test/java/com/jason/spring/cglib/PersonServiceTest.java
+++ src/test/java/com/jason/spring/cglib/PersonServiceTest.java
@@ -1,6 +1,13 @@
 package com.jason.spring.cglib;
 
+import net.sf.cglib.beans.BeanGenerator;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.FixedValue;
+import net.sf.cglib.proxy.MethodInterceptor;
 import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import static org.junit.Assert.*;
 
@@ -8,59 +15,57 @@ public class PersonServiceTest {
 
     @Test
     public void test1() {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(PersonService.class);
+        enhancer.setCallback((FixedValue) () -> "Hello Tom!");
+        PersonService proxy = (PersonService) enhancer.create();
 
-//        Enhancer enhancer = new Enhancer();
-//        enhancer.setSuperclass(PersonService.class);
-//        enhancer.setCallback((FixedValue) () -> "Hello Tom!");
-//        PersonService proxy = (PersonService) enhancer.create();
-//
-//        String res = proxy.sayHello(null);
-//
-//        assertEquals("Hello Tom!", res);
+        String res = proxy.sayHello(null);
+
+        assertEquals("Hello Tom!", res);
+
+        PersonService ps = new PersonService();
+        System.out.println(ps.sayHello(null));
+//        System.out.println(proxy.lengthOfName("1234"));
     }
 
     @Test
     public void test2() {
-//        Enhancer enhancer = new Enhancer();
-//        enhancer.setSuperclass(PersonService.class);
-//        enhancer.setCallback((MethodInterceptor) (obj, method, args, proxy) -> {
-//            if (method.getDeclaringClass() != Object.class && method.getReturnType() == String.class) {
-//                return "Hello Tom!";
-//            } else {
-//                return proxy.invokeSuper(obj, args);
-//            }
-//        });
-//
-//        PersonService proxy = (PersonService) enhancer.create();
-//
-//        assertEquals("Hello Tom!", proxy.sayHello(null));
-//        int lengthOfName = proxy.lengthOfName("Mary");
-//
-//        assertEquals(4, lengthOfName);
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(PersonService.class);
+        enhancer.setCallback((MethodInterceptor) (obj, method, args, proxy) -> {
+            System.out.println(obj.getClass());
+            System.out.println(method);
+            System.out.println(args[0]);
+            System.out.println(proxy);
+            System.out.println("=======");
+            if (method.getDeclaringClass() != Object.class && method.getReturnType() == String.class) {
+                return "Hello Tom!";
+            } else {
+                return proxy.invokeSuper(obj, args);
+            }
+        });
+
+        PersonService proxy = (PersonService) enhancer.create();
+
+        assertEquals("Hello Tom!", proxy.sayHello(null));
+        int lengthOfName = proxy.lengthOfName("Mary");
+
+        assertEquals(4, lengthOfName);
     }
 
     @Test
-    public void test3() {
-//        public interface Interface1 {
-//            String first();
-//        }
-//
-//        public interface Interface2 {
-//            String second();
-//        }
-//
-//        public class Class1 implements Interface1 {
-//            @Override
-//            public String first() {
-//                return "first behaviour";
-//            }
-//        }
-//
-//        public class Class2 implements Interface2 {
-//            @Override
-//            public String second() {
-//                return "second behaviour";
-//            }
-//        }
+    public void test3() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        BeanGenerator beanGenerator = new BeanGenerator();
+
+        beanGenerator.addProperty("name", String.class);
+        Object myBean = beanGenerator.create();
+        Method setter = myBean.getClass().getMethod("setName", String.class);
+        System.out.println(myBean.getClass());
+        setter.invoke(myBean, "some string value set by a cglib");
+
+        Method getter = myBean.getClass().getMethod("getName");
+        assertEquals("some string value set by a cglib", getter.invoke(myBean));
+        System.out.println(beanGenerator.getNamingPolicy().getClass());
     }
 }
